016e799a5a35f94846d40ba0df3b5aa0b9d43b53
Jhon <jhonb3r@gmail.com>
2018-09-13 17:21:55 -0500

    Set start time and handle post request

--- src/components/contestForm.js
+++ src/components/contestForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import utils from './utils';
+import Utils from './utils';
 import { Alert, Form, FormGroup, Button, Label, Input, FormText, FormFeedback } from 'reactstrap'
+var moment = require('moment');
 
 var config = require('../config-dev.json');
 var url = config.url_base;
@@ -28,10 +29,35 @@ class ContestForm extends Component {
     var val = this.state.minutesBeforeStart;
 
     if (isNumber(val) && val >= 5 && val <= 60) {
-      this.setState({valid: true, created: true});
+      url = config.url_backend + '/contest';
+      var start_time = moment(new Date()).add(this.state.minutesBeforeStart, 'm').toDate();
+
+      var data = {
+        name: this.props.contestName,
+        code: this.props.contestCode,
+        duration: 5,
+        startTime: start_time,
+        user: window.localStorage.user
+      }
+
+      console.log("url: ", url)
+      console.log("data: ", data);
+
+      var what = this;
+      Utils.postRequest(url, data, function (err, res) {
+        if (!err) {
+          what.setState({ valid: true, created: true });
+          // TODO: show info after successful created
+          console.log('res: ', res);
+        }
+        else {
+          console.log('XXXX', res)
+          alert(res);
+        }
+      });
     }
     else {
-      this.setState({valid: false});
+      this.setState({ valid: false });
     }
   }
 
@@ -49,12 +75,12 @@ class ContestForm extends Component {
         </p>
       </Alert>
 
-      <Form  style={{ textAlign: 'center' }}>
+      <Form style={{ textAlign: 'center' }}>
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           <Label for="minutes" className="mr-sm-2">Minutes before contest start</Label>
 
           <Input invalid={!this.state.valid} type="number" name="minutes" id="minutes" value={this.state.minutesBeforeStart}
-            onChange={(event) => {this.handleChange(event)}}
+            onChange={(event) => { this.handleChange(event) }}
           />
           <FormFeedback> Invalid value, enter a valid number. Example: 10</FormFeedback>
           <FormText>(Minimum 5 / Maximun 60 Minutes)</FormText>
