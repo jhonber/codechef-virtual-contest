5bb442386b3aa813a92a82c45a13086c7128d561
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2018-09-21 12:47:26 -0500

    refactor ternary operator

--- src/App.js
+++ src/App.js
@@ -54,21 +54,23 @@ class App extends Component {
       </p>
       : <p className='text-warning'> Anonymous </p>)
 
-    var loginButton = (!this.state.logged ? <Button
-      color='primary'
-      onClick={() => {
-        const callbackURL = url + Utils.config.urlAuthorize + '?response_type=code&client_id=' +
-          Utils.config.clientID + '&state=xyz&redirect_uri=' + Utils.config.urlRedirect
+    let loginButton = null
+    if (!this.state.logged) {
+      loginButton = <Button
+        color='primary'
+        onClick={() => {
+          const callbackURL = url + Utils.config.urlAuthorize + '?response_type=code&client_id=' +
+            Utils.config.clientID + '&state=xyz&redirect_uri=' + Utils.config.urlRedirect
 
-        window.location = callbackURL
-        Utils.moveTo(callbackURL)
-      }}
-    >
-      Login
-    </Button>
-      : null)
+          window.location = callbackURL
+          Utils.moveTo(callbackURL)
+        }}
+      >
+        Login
+      </Button>
+    }
 
-    var home = <div>
+    let home = <div>
       <div style={{ justifyContent: 'center', alignItems: 'center' }}>
         <Jumbotron fluid style={{ textAlign: 'center', padding: 15 }}>
           <Container fluid>
