ec4fc3a59b1a7f211a6dc20d0320a2f35d3ca9cb
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2018-09-25 18:21:53 -0500

    add contest registration

--- src/components/contest.js
+++ src/components/contest.js
@@ -38,7 +38,22 @@ class Contest extends Component {
     this.getUserContests(page)
   }
 
+  registerInContest = (contestID) => {
+    const registerURL = `${Utils.config.urlBackend}/contests/${contestID}/register`
+    Utils.postRequest(
+      registerURL,
+      { contestID: contestID, minutesBeforeStart: 5 },
+      function (err, res) {
+        // TODO: show this results on the UI
+        if (err) return console.log('can not register in contest', err)
+        if (res.error) return console.log('can not register in contest:', res.error)
+        console.log('successfully registered on contest', res)
+      }
+    )
+  }
+
   render () {
+    const self = this
     const items = this.state.contestList.map(function (i) {
       const duration = parseInt(i.duration, 10) / (1000 * 60 * 60)
       return (<tr key={i._id} >
@@ -51,7 +66,7 @@ class Contest extends Component {
         <td style={{ padding: 0, verticalAlign: 'middle' }}>
           <Button
             color='link'
-            onClick={() => { /* TODO: Set url to contest registration */ }} >
+            onClick={() => { self.registerInContest(i._id) }} >
             Register
           </Button>{' '}
         </td>
